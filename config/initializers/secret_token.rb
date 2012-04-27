c7356b438c14a38c79f9bab322e08ec69d677016
ewright <firefly2004@mac.com>
2012-04-26 18:31:54 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeftOverPie::Application.config.secret_token = '048f505383600675bb936b4933c14d47db94e54faaacee5cfd09f187dd22ac19bffaf2a5198ad391dc7cd7856cbc8577d74e0cd5e14375319dd28f3e6a7607ba'
